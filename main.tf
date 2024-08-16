2abe0134ef0bb7c4891296eaf5098f074f6bcd10
mjguru1996 <mailmemjguru@gmail.com>
2024-08-16 17:23:10 +0530

    Teraa5

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ resource "aws_security_group" "instance_sg" {
     cidr_blocks = [" 0.0.0.0/0" ]
    }
    egress = {
-     from_port = 0
+    from_port = 0
     to_port = 0
     protocol = "-1"
     cidr_blocks = [" 0.0.0.0/0" ] 
