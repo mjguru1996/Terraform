e01e2f4aafab9e6b80238201788dc4261658eebc
mjguru1996 <mailmemjguru@gmail.com>
2024-08-16 17:27:37 +0530

    Terap

--- main.tf
+++ main.tf
@@ -14,14 +14,14 @@ resource "aws_security_group" "instance_sg" {
     from_port = 22
     to_port = 22
     protocol = "tcp"
-    cidr_blocks = [" 0.0.0.0/0" ]
+    cidr_blocks = ["0.0.0.0/0"]
    }
    egress {
     from_port = 0
     to_port = 0
     protocol = "-1"
-    cidr_blocks = [" 0.0.0.0/0" ] 
-    
+    cidr_blocks = ["0.0.0.0/0"] 
+
    }
 }
 resource "aws_instance" "ec2-instance" {
